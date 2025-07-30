821f5ee4d6025cf008c619699f351aeb861c5180
Areeb Malik <areeb03@umd.edu>
2025-07-29 21:15:22 -0400

    Logo with name is done, working on navbar and band member specific page

---
+++ script.js
@@ -0,0 +1,6 @@
+// const container = document.getElementById("logo-container").getBoundingClientRect();
+// console.log(container);
+// const w = window.innerWidth;
+// const h = window.innerHeight;
+
+// console.log(`${w}px × ${h}px`);
